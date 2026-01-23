c160f49aaf8e2bd11785a5083ff4906f39564ac1
Orel Cosseron <orel.cosseron@ens-lyon.fr>
2026-01-23 09:36:08 +0100

    MazeContext instantiated in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 
-import { useState } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import Hud from '@/components/Hud'
 import Maze from '@/components/Maze'
 import MazeSelector from '@/components/MazeSelector'
 import { SessionContext } from '@/components/context/sessionContext'
+import { MazeContext, MazeContextType } from '@/components/context/mazeContext'
+import MazeToJSON from '@/src/MazeToJSON'
 
 export default function Home() {
   const [currentMazeName, setCurrentMazeName] = useState("")
@@ -16,14 +18,23 @@ export default function Home() {
   const [isWin, setIsWin] = useState(false)
   const sessionContext = { blockStack: blockStack, setBlockStack: setBlockStack, playerPos: playerPos, setPlayerPos: setPlayerPos, prevPlayerPos: prevPlayerPos, setPrevPlayerPos: setPrevPlayerPos, lines: lines, setLines: setLines, isWin: isWin, setIsWin: setIsWin }
 
+  const [mazeData, setMazeData]: [MazeContextType | undefined, Dispatch<SetStateAction<MazeContextType | undefined>>] | undefined = useState<MazeContextType | undefined>(undefined)
+
+  useEffect(() => {
+    if (currentMazeName)
+      setMazeData(MazeToJSON(currentMazeName.toLowerCase().replaceAll(" ", "_")))
+  }, [currentMazeName])
+
   return (
     <>
       <MazeSelector onChange={(name: string) => setCurrentMazeName(name)} />
       <SessionContext value={sessionContext}>
-        <Hud>
-          {currentMazeName !== "" && <Maze name={currentMazeName} />}
-          {/* <button id="reset_button">Reset</button> */}
-        </Hud>
+        <MazeContext value={mazeData}>
+          <Hud>
+            {mazeData && <Maze />}
+            {/* <button id="reset_button">Reset</button> */}
+          </Hud>
+        </MazeContext>
       </SessionContext>
     </>
   )
--- components/Maze.tsx
+++ components/Maze.tsx
@@ -1,41 +1,33 @@
 "use client"
 
-import { useState, useEffect, Dispatch, SetStateAction } from 'react'
-import { MazeContextType, MazeContext } from './context/mazeContext'
-import MazeToJSON from '@/src/MazeToJSON'
+import { useContext } from 'react'
+import { MazeContext } from './context/mazeContext'
 import Block from './Block'
 import Player from './Player'
 import Rim from './Rim'
 import Tile from './Tile'
 import Trophy from './Trophy'
 
-export default function Maze({ name }: { name: string }) {
-    const [mazeData, setMazeData]: [MazeContextType | undefined, Dispatch<SetStateAction<MazeContextType | undefined>>] | undefined = useState<MazeContextType | undefined>(undefined)
-
-    useEffect(() => {
-        setMazeData(MazeToJSON(name.toLowerCase().replaceAll(" ", "_")))
-    }, [name])
-
+export default function Maze() {
+    const mazeData = useContext(MazeContext)
     if (mazeData) {
         return (
-            <MazeContext value={mazeData}>
-                <div className="maze" style={{ width: mazeData.tile_size * (mazeData.rows[0].length / 2 + 1 / 2), height: mazeData.tile_size * (mazeData.rows.length + 1 / 2) }}>
-                    <Rim>
+            <div className="maze" style={{ width: mazeData.tile_size * (mazeData.rows[0].length / 2 + 1 / 2), height: mazeData.tile_size * (mazeData.rows.length + 1 / 2) }}>
+                <Rim>
 
-                        {Object.keys(mazeData.blocks).map(name => <Block key={name} name={name} />)}
+                    {Object.keys(mazeData.blocks).map(name => <Block key={name} name={name} />)}
 
-                        <Player />
+                    <Player />
 
-                        {mazeData.trophies.map((_, id) => <Trophy key={id} id={id} />)}
+                    {mazeData.trophies.map((_, id) => <Trophy key={id} id={id} />)}
 
-                        {mazeData.rows.map((row, i) =>
-                            row.match(/.{2}/g)?.map((_, j) =>
-                                <Tile key={i.toString() + "." + j.toString()} row={i} col={j} />
-                            )
-                        )}
-                    </Rim>
-                </div >
-            </MazeContext >
+                    {mazeData.rows.map((row, i) =>
+                        row.match(/.{2}/g)?.map((_, j) =>
+                            <Tile key={i.toString() + "." + j.toString()} row={i} col={j} />
+                        )
+                    )}
+                </Rim>
+            </div >
         )
     }
     return (<></>)
